a7336ea2d7c54219bb93e64dc5660b25d553bd22
Rplus <rplus.tw@gmail.com>
2018-11-08 09:32:37 +0800

    let it faster

--- check-position/main.js
+++ check-position/main.js
@@ -4,7 +4,7 @@ let checkPosition = () => {
     setPosition(e.coords);
     img.checker = setTimeout(() => {
       checkPosition();
-    }, (e.coords.accuracy < 100 ) ? 500 : 30000 );
+    }, (e.coords.accuracy < 100 ) ? 200 : 10000 );
   });
 };
 
